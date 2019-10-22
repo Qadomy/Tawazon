121c847607210cf70e6fe814dfa874adef7d8bc6
mac <ali.qadomy@hotmail.com>
2019-10-23 01:28:42 +0300

    Create a adapter class extend PagerAdapter, set a GIF images in viewPager

--- app/src/main/java/com/blue/tawazon/MainActivity.java
+++ app/src/main/java/com/blue/tawazon/MainActivity.java
@@ -1,14 +1,27 @@
 package com.blue.tawazon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.blue.tawazon.Adapter.CustomSwipAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CustomSwipAdapter adapter;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // init View Pager
+        viewPager = findViewById(R.id.viewPager);
+        adapter = new CustomSwipAdapter(this);
+
+        // set the images in view pager
+        viewPager.setAdapter(adapter);
     }
 }
